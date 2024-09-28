b0472228d332f92b4861e76ceb5451db7376f7f2
SamuelDMFerreira <samdmf@SamuelFLaptop>
2024-09-28 14:26:47 -0700

    finished functionality of wcat

--- project1/initial-utilities/wcat/wcat.cpp
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -5,6 +5,29 @@
 #include <iostream>
 #include <ostream>
 
+void printFile(const char* filePath)
+{
+	// check if file can be opened
+	int fileDescriptor = open(filePath, O_RDONLY);
+	if (fileDescriptor < 0)
+	{
+		std::cout << "wcat: cannot open file" << std::endl;
+		exit(1);
+	}
+	
+	// print out full file until end of file line
+	void *buffer = malloc(sizeof(char));
+	int bytesRead = sizeof(char);
+	while (read(fileDescriptor, buffer, bytesRead) > 0)
+	{
+		write(1, buffer, bytesRead);
+	}
+	free(buffer);	
+
+	close(fileDescriptor);	
+
+}
+
 int main(int argc, char* argv[])
 {
 	// If user doesn't specify parameters 
@@ -13,15 +36,9 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	
-	const char* filePath = argv[1];
-	// check if file can be opened
-	int fileDescriptor = open(filePath, O_RDONLY);
-	if (fileDescriptor < 0)
+	for (int argNum = 1; argNum < argc; argNum++)
 	{
-		std::cout << "wcat: cannot open file" << std::endl;
-		exit(1);
+		const char* filePath = argv[argNum];
+		printFile(filePath);
 	}
-
-	close(fileDescriptor);	
-
 }
