aa5044a53da4c512b74062c152ebdc44b5542d6a
SamuelDMFerreira <samdmf@SamuelFLaptop>
2024-10-05 14:43:28 -0700

    started working on LS

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
+using namespace std;
+
+void processCommand(string commandStr)
+{
+	// arguments for command
+	//char** argv;
+	stringstream cSStrm(commandStr);
+	string currW;
+	cSStrm >> currW;
+
+	if (commandStr == "exit") // this should be called first
+	{
+		exit(0);
+	}
+	else if (currW == "ls")
+	{
+		cout << "epic LS call" << endl;
+	}
+
+}
 
 int main(int argc, char* argv[])
 {
 	// error if more than 2 arguments
 	if (argc > 2)
 	{
-		std::cout << "./wish [batch file]" << std::endl;
+		cout << "./wish [batch file]" << endl;
 		return 1;
 	}
 	
 	
 	if (argc == 1)
 	{
-		std::string commandStr = "";
+		string commandStr = "";
 		// command fetching while loop
 		while(commandStr != "exit")
 		{
-			std::cout << "wish> ";
-			getline(std::cin, commandStr);	
+			cout << "wish> ";
+			getline(cin, commandStr);
+			processCommand(commandStr);
 		}
 	}
 	else
 	{
-		std::ifstream input;
+		ifstream input;
 		input.open(argv[1]);
-		std::string commandStr = "";
+		string commandStr = "";
 		// command fetching while loop
 		while(!input.eof() && commandStr != "exit")
 		{
-			getline(input, commandStr);	
+			getline(input, commandStr);
+			processCommand(commandStr);
 		}
 	}
-
-
-
 	return 0;
 }
